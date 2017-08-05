03e4aec480bbe0f683b8e42c5b66a323ed42fa64
Yoann Fleury <yoann.fleury@yahoo.com>
2017-08-05 11:53:22 +0200

    feat(output): add color for open or closed issue

--- src/body/Issue.ts
+++ src/body/Issue.ts
@@ -1,26 +1,36 @@
+const chalk = require('chalk');
+
 export interface Issue {
   title: string;
   body?: string;
   description?: string;
+  state: string;
 }
 
 export class IssueGithub implements Issue {
   public title: string;
   public body: string;
+  public state: string;
 }
 
 export class IssueGitlab implements Issue {
   public title: string;
   public description: string;
+  public state: string;
 }
 
 export function printIssue(issue: Issue): string {
+  let color = chalk.green;
+  if (issue.state === "closed") {
+    color = chalk.red;
+  }
+
   let delimiter = '+-----------------------------------------------------+';
 
   if (issue.body != undefined) {
-    return `${delimiter}\n${issue.title}\n${delimiter}\n${issue.body}`;
+    return color(`${delimiter}\n${issue.title}\n${delimiter}\n${issue.body}`);
   }
   if (issue.description != undefined) {
-    return `${delimiter}\n${issue.title}\n${delimiter}\n${issue.description}`;
+    return color(`${delimiter}\n${issue.title}\n${delimiter}\n${issue.description}`);
   }
 }
